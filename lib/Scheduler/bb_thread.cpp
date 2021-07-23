923d0863d0f4344236f57bbf14e7f279046ec800
jeff <jrbyrnes1989@gmail.com>
2021-07-23 15:42:31 -0700

    fixed best sched reporting bug

--- lib/Scheduler/bb_thread.cpp
+++ lib/Scheduler/bb_thread.cpp
@@ -1892,6 +1892,7 @@ FUNC_RESULT BBWorker::enumerate_(EnumTreeNode *GlobalPoolNode,
         if (*MasterImprvCount_ > 0) {
             RegionSchedLock_->lock(); 
               if (MasterSched_->GetSpillCost() < RegionSched_->GetSpillCost()) {
+                Logger::Info("updating best schedule");
                 RegionSched_->Copy(MasterSched_);
                 RegionSched_->SetSpillCost(MasterSched_->GetSpillCost());
               }
@@ -2244,6 +2245,7 @@ FUNC_RESULT BBWorker::enumerate2_(HalfNode *GlobalPoolNode,
             //Logger::Info("found an improved schedule");
             RegionSchedLock_->lock(); 
               if (MasterSched_->GetSpillCost() < RegionSched_->GetSpillCost()) {
+                Logger::Info("updating region schedule");
                 RegionSched_->Copy(MasterSched_);
                 RegionSched_->SetSpillCost(MasterSched_->GetSpillCost());
               }
@@ -3416,8 +3418,9 @@ FUNC_RESULT BBMaster::Enumerate_(Milliseconds startTime, Milliseconds rgnTimeout
   // TODO -- handle result -- store OptimalSolverID
   *OptimalSolverID = 1; //master schedule
   
-  if (enumBestSched_->GetSpillCost() < bestSched_->GetSpillCost())
+  if (enumBestSched_->GetSpillCost() < bestSched_->GetSpillCost() && *Enumrtr_->getImprvCnt() > 0)
   {
+    Logger::Info("master updating region sched");
     bestSched_ = enumBestSched_;
   }
 
