bb956b81742afd79f92fdb7321d649298f4202f9
Jeff H <130211779+Sanguine-Choleric@users.noreply.github.com>
2025-05-28 14:42:36 -0700

    Merge history bug fix into active dev branch (#8)
    * Init bug fix for bad history pruning
    - Undefined behavior in hash key calc resulted in (almost) no history pruning
    - 'Fix' adds `volatile` to hash key calc to avoid weird compiler behavior
    
    * Cleanup and suggested changes

--- include/opt-sched/Scheduler/hash_table.h
+++ include/opt-sched/Scheduler/hash_table.h
@@ -627,9 +627,15 @@ template <class T> inline void BinHashTable<T>::CmputConsts_() {
 
 template <class T>
 inline UDT_HASHVAL BinHashTable<T>::HashKey(UDT_HASHKEY key) {
-  if (keyBitCnt_ == hashBitCnt_)
-    return (UDT_HASHVAL)key;
-  return ((UDT_HASHVAL)key >> hashRShft_);
+
+  // FIXME: Without `volatile`, it seems like the compiler optimizes out parts
+  // of this function, leading to undefined behavior. Volatile prevents the
+  // compiler from making certain operations, so it's best if there's a better
+  // fix
+  volatile UDT_HASHVAL result = static_cast<UDT_HASHVAL>(key) >>
+                                (keyBitCnt_ == hashBitCnt_ ? 0 : hashRShft_);
+
+  return result;
 }
 
 template <class T>
